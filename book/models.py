b74075e453f0d5c6e043e203e0c9a20025bfb513
BlackPrince163 <niazhadik3@mail.ru>
2022-10-23 23:13:04 +0300

    + author, genre, book models

--- book/models.py
+++ book/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    author = models.ManyToManyField(Author)
+    genre = models.ManyToManyField(Genre)
+    price = models.FloatField(null=True, blank=True)
+    isbn = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.title
